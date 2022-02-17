00af17393e3d4dbfc1cfc2372d78b9ca03c9c4ae
Jeffrey Li <dr.cheddar@outlook.com>
2022-02-17 11:34:11 -0500

    pre nuke for matrix checkes

--- hi-world/src/main/scala/Polytopia/Polytopia.scala
+++ hi-world/src/main/scala/Polytopia/Polytopia.scala
@@ -24,7 +24,8 @@ object Polytopia {
 
     type Op[T] = T => T;
     type MP[E] = MultivariatePolynomial[E];
-    type Cartan = linalg.DenseMatrix[Int]
+    type Cartan = linalg.DenseMatrix[Int];
+    type Chain = Array[Int];
 
     type CartanPredicate = (Int, Int) => Int
     type CartanFamily = (Int) => CartanPredicate
@@ -86,6 +87,7 @@ object Polytopia {
     }
 
     implicit def p2O[E](p: MP[E]): Op[MP[E]] = (x) => x * p;
+    implicit def opArray2Op[A](arr: Array[Op[A]]): Op[A] = arr.reduce((a, b) => a --> b)
 
     implicit def polyWrapper[E](p: MP[E]) = new AnyRef {
         def -->(op: Op[MP[E]]): Op[MP[E]] = (x) => p * op(x)
@@ -150,178 +152,286 @@ object Polytopia {
     }
 
     def main() = {
-        implicit val dim = 5;
+        implicit val dim = 4;
         val rangle = Array.range(0, dim).map((k) => s"a_$k")
         implicit val ring = MultivariateRing(Q, rangle);
-        val rootSystem = RootSystems('K');
+        val rootSystem = RootSystems('F');
 
         implicit val c: Cartan = CartanMatrix(dim, rootSystem)
-        // println(c(::, 0))
-        // println(c(::, 1))
-        // println(c(::, 2))
 
-        // val cInv = linalg.inv(c);
-        // val detC = linalg.det(c).toInt;
-        // Bruh
-        // println(cInv)
+        val id = (x: ring.PolyType) => x
         val x_ = ring.generators
         val a_ = reflectionRoots(c)
         val s = reflections(a_)
         val y_ = x_.mapWithIndex((x, i) => s(i)(x))
 
-        val D = s.zipWithIndex.map { case (refl, index) =>
-            Differential(a_)(ring)(refl, index)
-        };
-        // var masterProduct = ring.one;
+        val D = s.mapWithIndex((refl, index) => Differential(a_)(ring)(refl, index));
 
-        // print(c)
-
-        // val  weightDiffs =  linalg.DenseMatrix.tabulate(3, 3){case (i, j) => D(i)(x_(j))}
-
-        // println(weightDiffs)
-
-        var monos = Array.ofDim[Int](dim).mapWithIndex((_, i) => x_(i) ^ (2 * i + 1))
-        var chains = Array.ofDim[Int](dim).mapWithIndex((_, i) => D(i))
-        for (i <- 1 until dim) {
-            chains(i) = (D(i) --> chains(i - 1) --> D(i))
-        }
-        var sumBrackets = Array
-            .ofDim[Int](dim)
-            .mapWithIndex((_, i) =>
-                (p: ring.PolyType) => chains(i)(x_(i) * p) + x_(i) * chains(i)(p)
-            )
-
-        // println(( D(3) --> chains(2))( x_(2)* D(3)(monos(3)) ))
-        //
-        // println(sumBrackets(1)(monos(1)))
-        // println(sumBrackets(0)(monos(0)))
-        val id = (x: ring.PolyType) => x
-
-        // x_i k_i + k_i x_i (x_i)^{2i-1} = x_i + 2i x_{i+1}
-        val targetK = (i: Int) => chains(i) --> x_(i)
-        // print("Target: ")
-        // println(targetK(1)(monos(1)))
-
-        // val step = (i:Int) => D(i) --> chains(i-1) --> D(i) --> x_(i)
-        // val step = (i:Int) => D(i) --> chains(i-1) --> (p2O(y_(i)) --> D(i)  + id)
-        // val step = (i:Int) => D(i) --> chains(i-1) --> (p2O(x_(i-1) - x_(i)  + x_(i+1)) --> D(i)  + id)
-        // val step = (i:Int) => (x_(i+1) --> chains(i)) - (D(i)-->x_(i) -->chains(i-1)-->D(i))+ (D(i) --> chains(i-1) --> x_(i-1) --> D(i)) + (D(i) --> chains(i-1) )
-        // val step = (i:Int) => (x_(i+1) --> chains(i)) - ((y_(i) --> D(i) + id) -->chains(i-1)-->D(i))+ (D(i) --> chains(i-1) --> x_(i-1) --> D(i)) + (D(i) --> chains(i-1) )
-        // var step = (i:Int) => ((x_(i+1) - y_(i)) --> chains(i)) - (chains(i-1)-->D(i)) + (D(i) --> chains(i-1) --> x_(i-1) --> D(i)) + (D(i) --> chains(i-1) )
-        // var step = (i:Int) => ((x_(i) - x_(i-1)) --> chains(i)) + (D(i) --> chains(i-1) --> x_(i-1) --> D(i)) + (D(i) --> chains(i-1) ) - (chains(i-1)-->D(i))
-        // var step = (i:Int) => ((x_(i) - x_(i-1)) --> chains(i)) + (D(i) --> (Diracket(chains(i-1), x_(i-1)) + (x_(i-1) --> chains(i-1))) --> D(i)) + (D(i) --> chains(i-1) ) - (chains(i-1)-->D(i))
-        // var step = (i:Int) => ((x_(i) - x_(i-1)) --> chains(i)) +  (x_(i-1) --> chains(i))  + (D(i) --> (Diracket(chains(i-1), x_(i-1))) --> D(i)) + (D(i) --> chains(i-1) ) - (chains(i-1)-->D(i))
-        var step = (i: Int) =>
-            ((x_(i)) --> chains(i)) + (D(i) --> (Diracket(chains(i - 1), x_(i - 1))) --> D(i)) + (D(
-              i
-            ) --> chains(i - 1)) - (chains(i - 1) --> D(i))
-
-        // println(x_(2)- y_(1))
-        // print("step: ")
-        // println(step(1)(monos(1)))
-
-        val stepNumber = 3
-
-        val dirac0 = (2 * (x_(1) - x_(0)) --> D(0)) + id
-        // [k_0 x_0 - x_0 k_0]
-
-        val targetDirac = (i: Int) => Diracket(chains(i), x_(i))
-
-        // print(f"Target ${stepNumber}:")
-        // println(targetDirac(stepNumber)(monos(stepNumber)))
-
-        // var dirac1Step = (D(1) --> dirac0 --> D(1)) + Diracket(D(1), chains(0))
-        // var dirac1Step = (D(1) --> 2*(x_(1) - x_(0)) --> D(0)  --> D(1)) + Diracket(D(1), chains(0))
-        // var dirac1Step = ((D(1) --> x_(1) --> D(0)  --> D(1)) - (x_(0) --> chains(1)))*2 + Diracket(D(1), chains(0))
-        // var dirac1Step = ((((y_(1) --> D(1)) + id) --> D(0)  --> D(1)) - (x_(0) --> chains(1)))*2 + Diracket(D(1), chains(0))
-        // var dirac1Step = (( D(0)  --> D(1)) + ((y_(1) - x_(0)) --> chains(1)))*2 + Diracket(D(1), chains(0))
-        // var dirac1Step = (( D(0)  --> D(1)) + ((x_(2)- x_(1)) --> chains(1)))*2 + Diracket(D(1), chains(0))
-        // var dirac1Step = ( D(0)  --> D(1))*2 + ((x_(2)- x_(1)) --> chains(1))*2 + Diracket(D(1), chains(0))
-        var dirac1 = (((x_(2) - x_(1)) --> chains(1)) * 2 + Addracket(D(1), chains(0)))
-        // [k_1 x_1 - x_1 k_1]
-
-        // var
-
-        // var dirac2Step =  (D(2) --> dirac1 --> D(2)) + Diracket(D(2), chains(1))
-        // var dirac2Step =  (D(2) --> (((x_(2)- x_(1)) --> chains(1))*2 + Addracket(D(1), chains(0))) --> D(2)) + Diracket(D(2), chains(1))
-        // var dirac2Step =  (D(2) --> (((x_(2)- x_(1)) --> chains(1))*2 + Addracket(D(1), chains(0))) --> D(2)) + Diracket(D(2), chains(1))
-        var dirac2Step = ((2 * (x_(3) - x_(2))) --> chains(2)) + Addracket(D(2), chains(1)) + (D(
-          2
-        ) --> Addracket(D(1), chains(0)) --> D(2))
-        // var dirac2Step = Addracket(D(2), chains(1)) + (D(2) --> Addracket(D(1), chains(0)) --> D(2))
-        var dirac3Step = Addracket(D(3), chains(2))
-
-        // print(s"D Step ${stepNumber}:")
-        // println(dirac3Step(monos(3)))
-        // should be equal to (k_i x_i p)
-
-        // println(kp(1)(monos(1)))
-
-        // println((D(1)--> D(0))(x_(0)* D(1)(x_(1)^3)))
-
-        println("HII")
-
-        def reeChain(i: Int): Op[ring.PolyType] = {
-            var ree = D(0);
-            for (j <- 0 until i) {
-                var k = i - j
-                ree = ree --> D(k)
+        val xd = Array(Array(0), Array(1), Array(2), Array(3));
+        def getPermutations(n: Int): Array[Array[Int]] = {
+            if (n == 1) return xd
+            else {
+                val prev = getPermutations(n - 1)
+                return xd.map(k => prev.map(s => s ++ k)).reduce((a, b) => a ++ b)
             }
-            for (j <- 2 to i) {
-                ree = ree --> D(j)
-            }
-            return ree
         }
 
-        // reeChain(2)
-        println((D(1) --> D(2))(x_(2) ^ 5))
-        println((D(2) --> D(1) --> D(2))(x_(2) ^ 5))
+        // println(getPermutations(1).length)
+        // println(getPermutations(2).length)
 
-        // println(reeChain(2)(x_(2)^5))
+        def doTestingThings(p: ring.PolyType) : Array[Array[Int]] = {
+            var ones = Array[Array[Int]]()
+            for (chain <- getPermutations(p.degree())) {
+                val dunno = opArray2Op(chain.map(i => D(i)))
+                val result = dunno(p)
+                if (result == ring.one) {
+                    ones = ones :+ chain;
+                }
+            }
+            return ones;
+        }
 
-        // // println((chains(2)) (monos(2) * x_(2)))
-        // // K3 (x3^6)
-        // println((chains(1)) (y_(2)* D(2)(monos(2))))
-        // println((chains(1)) ( D(2)(monos(2))))
+        val maxs = Array[ring.PolyType](
+            (x_(0)^1) * (x_(1)^2),
+            (x_(2))^4,
+            (x_(3))^6
+        );
 
-        // // K2 (y3 * D3 x3^5)
-        // // 4z3z4 + 2(z3)^2 - 2 z2 z3
-        // // (2i-2)x_(i)x_i+1 + 2 (x_i)^2 - 2 x_(i-1)x_i
-
-        // println((chains(1)) (x_(1)* D(2)(monos(2))))
-        // println((chains(1)) ((-x_(2)+x_(3))* D(2)(monos(2))))
-
-        // // K2 (-x3 * D3 x3^5)
-        // //
-        // println((D(2) --> chains(1)) (y_(2)* D(2)(monos(2))))
-        // D3 K2 (y3 * D3 x3^5)
-
-        // println(chainer(3)._1 (chainer(3)._2))
-
-        // for(i <- chainer)
+        // val maxsOnesTested = maxs.map(doTestingThings)
+        // for(icu <- 0 until maxs.length)
         // {
-        //     print(i._2)
-        //     print(" ")
+        //     val chainOfChains = maxsOnesTested(icu)
+        //     if(chainOfChains.length == 0)
+        //     {
+        //         println("YAY INDEX :" + icu)
+        //     }
+        //     else
+        //         {
+        //             println("FUCK INDEX: " + icu)
+        //             println(chainOfChains.length)
+        //             println(chainOfChains(0).mkString(" "))
+        //         }
         // }
 
-        // println(y_(3))
-        // println((chainer(2)._1)(y_(3)^5))
-        /** [ (d_0, x_0), (d_1 d_0 d_1, x_1^3), ... ]
+        // var chains = Array[Chain](
+        //     Array(0),
+        //     Array(0,1),
+        //     Array(2,1,2),
+        //     Array(3,2,1,2,3)
+        // );
+
+        def PartialChain(ch: Chain) = ch.map(D);
+        def MonoChain(ch: Chain) = (x_(ch.last) ^ (ch.length));
+        var chains = Array[Chain](
+          Array(1, 0),
+          Array(1),
+          Array(2, 1, 2),
+          Array(3, 2, 1, 2, 3)
+        );
+        var monos = chains.map(MonoChain)
+        var partialChains = chains.map(PartialChain);
+        // println(partialChains(0)(monos(0)))
+
+        // var weirdBoi = id --> partialChains(0) --> (partialChains(3)) --> (partialChains(2)) --> (partialChains(1))
+        // var total = monos.reduce((a, b) => a*b);
+        // println(weirdBoi(total)) - reduces to 1
+
+        // var andy = D(2) --> D(1);
+        // println(andy(x_(1)^2)) // D1 D2 x_1^2 when x_1 > x_2 reduces to 2
+
+        // var randy = Array(0,1,2)
+        // println("randy: " + PartialChain(randy)(MonoChain(randy) ));
+
+        // var sandy = Array(2,1,2,3)
+        // println("sandy: " + PartialChain(sandy)(MonoChain(sandy) ));
+
+        val sM = s.mapWithIndex((_, i) =>
+            linalg.DenseMatrix
+                .eye[Int](dim)
+                .mapPairs((p, v) =>
+                    if (p._2 == i)
+                        if (p._1 == p._2) -1
+                        else -c(p._2, p._1)
+                    else if (p._1 == p._2) 1
+                    else 0
+                )
+        );
+
+        
+        // switch operators as matrix on basis of x_i
+
+        val xV = x_.mapWithIndex((_, i) =>
+            linalg.DenseVector
+                .zeros[Double](dim)
+                .mapPairs((j, _) =>
+                    if (i == j) 1
+                    else 0
+                )
+        )
+
+        println(sM(0))
+        println(" ")
+        println(sM(1))
+        println(" ")
+        // println(sM(2))
+        // println(" ")
+        // println(sM(3))
+        // println(" ")
+        println(sM(0)*sM(1));
+        println(" ")
+
+        println(sM(1)*sM(0)*sM(1));
+        println(" ")
+        
+        println(sM(1)*sM(0));
+        println(" ") 
+        
+
+        
+
+        // D(i) M x(j) = M(i,j)
+
+        // println((s(1) --> s(2) --> s(1))(x_(1)))
+
+        // println(sM(1) * sM(2) *sM(1) * xV(1))
+
+        val mId = linalg.DenseMatrix.eye[Int](dim);
+
+        // if s_i s_2 s_j has a -1 in any row row k
+        // then \partial_k \partial_i \partial_j = 0
+        type mOp = linalg.DenseMatrix[Int]
+        type ZooResident = (Chain, mOp)
+        var zoo = Array[ZooResident]()
+        val zooEntrance = collection.mutable.PriorityQueue.empty[(Chain, mOp)](Ordering.by((_:(Chain, mOp))._1.length).reverse)
+        zooEntrance.addOne((Array[Int](), mId));
+
+        var negCount = 0
+        var dupCount = 0;
+
+        while(zooEntrance.nonEmpty)
+        {
+            val moo = zooEntrance.dequeue()
+            val found = zoo.find((k) => k._2 == moo._2)
+            if(found != None)
+            {
+                dupCount+=1;
+            }
+            if(found == None)
+            {
+                zoo +:= moo
+
+                for(i <- 0 until dim)
+                {
+                    // if(sM(i) * moo._2 != moo._2 * sM(i))
+                    // {
+                    // }
+                    val ithRow = moo._2(i,::);
+                    // val hasNeg = false
+                    val hasNeg = ithRow.t.toScalaVector().find((k) => (k < 0)) != None;
+                    if(hasNeg)
+                    {
+                        negCount+=1;
+                    }
+                    if(!hasNeg)
+                    {
+                        val k = (i +: moo._1,  sM(i) * moo._2)
+                        zooEntrance+= k
+                    }
+                }
+            }
+        }
+        println("negatives: " + negCount + " dups: " + dupCount)
+
+        /** Theory - chain (K) len (r) is zero iff there is other chain (K2) with len (r2) so that
+          * s_K = s_K2 but r > r2.
+          *
+          * D_K = 0 IFF <-> K is non-minimal / reducible. IFF len(K) > height(s_K)
+          *
+          * So then if you have D_i on the matrix with negative numbers it means that there is
+          * smaller chain that produces the same reflection in the Weyl group.
+          *
+          * Like basically the negative number in whatever ith row is telling you basically that its
+          * already reflection conjugate under s_i
+          *
+          * Like for example s_1 (s_2 s_1 s_2) = s_2 s_1
+          *
+          * For the ones with negative in the row - does s_i positive it?
+          *
+          * Theorem - if you apply s_i on a chain matrix where there is neg in ith row, Then there
+          * is smaller K2 which gives you same refl group element.
+          *
+          * // println("negatives: " + negCount + " dups: " + dupCount)
+          *
+          * 2304 negatives (2 * 1152) and 1153 dups (1152 + 1)
+          * --> The +1 comes from because you first populate the zooEntrance with Identity, so it
+          * double counts Id, and thats all.
+          *
+          * negs is where you have an equal chain of strictly smaller size dups is then where you
+          * have an equal chain of strictly equal size
+          *
+          * Without the (hasNeg) filter, you get 3458 (= 1154 + 2304) dups. Which means that every
+          * single chain that failed the hasNeg filter was a dup. hasNeg therefore must be the same
+          * thing as <--> !irreducible
+          *
+          * Assume you have chain K_r. Assume D_j s_K x_i < 0 for some i, j. Assume S(K_r) {i,j}
+          * element at {i,j} in the S(K_r) matrix is negative Then s_j S(K_r) = S(K_r2) for some
+          * chain K_r2 with r2 < r1.
           */
-        // val p1 = D(0)
-        // val p2 = D(0) --> D(1) --> D(0);
-        // val p3 = D(0) --> D(1) --> D(2) --> D(3) --> D(2) --> D(1) --> D(0)
-        // val p4 = D(0) -> D
 
-        // println((D(0))(x_(0) ^ 7))
-        // val p3 = D(0) --> D(1) --> D(2) --> D(1) --> D(0);
-        // println(D(0)( x_(0)^5 ))
+        // var tested = 0;
+        // for(w <- zoo)
+        // {
+        //     for(i <- 0 until dim)
+        //     {
+        //         var negTested = false;
+        //         for(j <- 0 until dim)
+        //         {
+        //             if(w._2(i,j) < 0 && !negTested)
+        //             {
+        //                 val sw = sM(i) * w._2;
+        //                 val minimal = zoo.find(k => k._2 == sw);
+        //                 if(minimal==None)
+        //                 {
+        //                     throw new Exception("reeeee")
+        //                 }
+        //                 else
+        //                 {
+        //                     if(w._1.length  > minimal.get._1.length)
+        //                     {
+        //                         // yay
+        //                         negTested = true;
+        //                         tested+=1;
+        //                     }
+        //                     else
+        //                         {
+        //                             println("FUCKKKK")
+        //                             println("i: " + i)
+        //                             println("w: " + w._1.mkString(" "))
+        //                             println("wmin: " + minimal.get._1.mkString(" "))
+        //                             throw new Exception("FUCKKKKKKKKK")
+        //                         }
+        //                 }
+        //                 // if(minimal != None && minimal.)
+        //             }
+        //         }
+        //     }
+        // }
+        // println(" YAY : " + tested )
+        // println(zoo.length)
+        // // zoo.
+        // println(zoo(0)._1.mkString(" "))
+        // println(zoo(0)._1.length)
 
-        // val k = s(1) --> s(0)
-        // println(Refactor((  D(0)(x_(2))   )))
-        // println(Refactor(x_(0)))
-        // print(Refactor((D(1))(v_(2) * v_(1))))
-        // print(Refactor(D(1)(v_(2)) * v_(1) + s(1)(v_(2)) * D(1)(v_(1))))
+        // val row0s = zoo.map((k) => k._2.t(::,0)).distinct
+        // println(row0s.length)
+        // println(row0s.mkString("\n"))
+        // println(zoo(4))
+        // print(row0s(4))
+
+        // println((mId - sM(1))*sM(0))
+
+        // Theory - if p is unimodular, then every factor of p is also unimodular
+
     }
 
 }
